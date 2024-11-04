d202e46d2da1635375f60976b7d4cf397223dd20
TusharAbhinav <abhinav.tushar@iesoftek.com>
2024-11-04 12:03:07 +0530

    add dns forward server

--- app/main.go
+++ app/main.go
@@ -4,19 +4,23 @@ import (
 	"bytes"
 	"encoding/binary"
 	"fmt"
-	"github.com/codecrafters-io/dns-server-starter-go/app/sections/answer"
+	"net"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+	//"github.com/codecrafters-io/dns-server-starter-go/app/sections/answer"
 	"github.com/codecrafters-io/dns-server-starter-go/app/sections/header"
 	"github.com/codecrafters-io/dns-server-starter-go/app/sections/question"
-	"net"
 )
 
 // |  Bit  |  7  |  6  |  5  |  4  |  3  |  2  |  1  |  0  |
 // |-------|-----|-----|-----|-----|-----|-----|-----|-----|
 // | Value |  0  |  0  |  0  |  0  | Opcode  |  AA  |  TC  |  RD  |
 
-func createHeaderSection(buf []byte) ([]byte, error) {
+func createHeaderSection(buf []byte, ANCOUNT uint16) ([]byte, error) {
 	buffer := bytes.NewReader(buf[:2])
-	var ID, QDCOUNT, ANCOUNT, OPCODE, RD, RCODE uint16
+	var ID, QDCOUNT, OPCODE, RD, RCODE uint16
 
 	idErr := binary.Read(buffer, binary.BigEndian, &ID)
 	if idErr != nil {
@@ -34,7 +38,6 @@ func createHeaderSection(buf []byte) ([]byte, error) {
 
 	// Set QDCOUNT and ANCOUNT
 	QDCOUNT = binary.BigEndian.Uint16(buf[4:6])
-	ANCOUNT = QDCOUNT
 	// Set RCODE based on OPCODE
 	if OPCODE == 0 {
 		RCODE = 0
@@ -134,55 +137,122 @@ func createQuestionSection(buf []byte) []byte {
 	return questionResponse
 }
 
-func createAnswerSection(buf []byte, answerCount int) ([]byte, error) {
-	answerResponse := make([]byte, 0)
+// func createAnswerSection(buf []byte, answerCount int) ([]byte, error) {
+// 	answerResponse := make([]byte, 0)
 
-	for i := 0; i < answerCount; i++ {
-		name := createLabel(0, buf)
+// 	for i := 0; i < answerCount; i++ {
+// 		name := createLabel(0, buf)
 
-		typeBuff := make([]byte, 2)
-		binary.BigEndian.PutUint16(typeBuff, 1)
+// 		typeBuff := make([]byte, 2)
+// 		binary.BigEndian.PutUint16(typeBuff, 1)
 
-		classBuff := make([]byte, 2)
-		binary.BigEndian.PutUint16(classBuff, 1)
+// 		classBuff := make([]byte, 2)
+// 		binary.BigEndian.PutUint16(classBuff, 1)
 
-		ttlBuff := make([]byte, 4)
-		binary.BigEndian.PutUint32(ttlBuff, 60)
+// 		ttlBuff := make([]byte, 4)
+// 		binary.BigEndian.PutUint32(ttlBuff, 60)
 
-		lengthBuff := make([]byte, 2)
-		binary.BigEndian.PutUint16(lengthBuff, 4)
+// 		lengthBuff := make([]byte, 2)
+// 		binary.BigEndian.PutUint16(lengthBuff, 4)
+// 		ipStart := len(name) + 2 + 2 + 4 + 2
+// 		ipAddress := buf[ipStart : ipStart+4]
+// 		answerObj := answer.Answer{
+// 			Name:   name,
+// 			Type:   typeBuff,
+// 			Class:  classBuff,
+// 			TTL:    ttlBuff,
+// 			Length: lengthBuff,
+// 			Data:   ipAddress,
+// 		}
 
-		ipAddress := net.ParseIP("8.8.8.8").To4()
+// 		answerResponse = append(answerResponse, answerObj.Name...)
+// 		answerResponse = append(answerResponse, answerObj.Type...)
+// 		answerResponse = append(answerResponse, answerObj.Class...)
+// 		answerResponse = append(answerResponse, answerObj.TTL...)
+// 		answerResponse = append(answerResponse, answerObj.Length...)
+// 		answerResponse = append(answerResponse, answerObj.Data...)
+// 	}
 
-		answerObj := answer.Answer{
-			Name:   name,
-			Type:   typeBuff,
-			Class:  classBuff,
-			TTL:    ttlBuff,
-			Length: lengthBuff,
-			Data:   ipAddress,
-		}
-
-		answerResponse = append(answerResponse, answerObj.Name...)
-		answerResponse = append(answerResponse, answerObj.Type...)
-		answerResponse = append(answerResponse, answerObj.Class...)
-		answerResponse = append(answerResponse, answerObj.TTL...)
-		answerResponse = append(answerResponse, answerObj.Length...)
-		answerResponse = append(answerResponse, answerObj.Data...)
+//		return answerResponse, nil
+//	}
+func connectResolver(packet []byte, resolverIP string, resolverPort int) ([]byte, int) {
+	resolverAddr := &net.UDPAddr{
+		IP:   net.ParseIP(resolverIP),
+		Port: resolverPort,
 	}
 
-	return answerResponse, nil
+	conn, err := net.DialUDP("udp", nil, resolverAddr)
+	if err != nil {
+		fmt.Println("Error connecting to resolver:", err)
+		return []byte{}, 9
+	}
+	fmt.Println("new connection established")
+
+	_, err = conn.Write(packet)
+	if err != nil {
+		fmt.Println("Error sending to resolver:", err)
+		return []byte{}, 0
+	}
+
+	conn.SetReadDeadline(time.Now().Add(2 * time.Second))
+
+	response := make([]byte, 512)
+	n, _, err := conn.ReadFromUDP(response)
+	if err != nil {
+		fmt.Println("Error reading from resolver:", err)
+		return []byte{}, 0
+	}
+	conn.Close()
+	return response, n
+}
+func forwardDNSServer(buf []byte, resolverAddr string) [][]byte {
+	header := buf[0:12]
+	header[2] &= 0b01111111
+	qdCount := binary.BigEndian.Uint16(buf[4:6])
+	totalQuestions := [][]byte{}
+	totalAnswers := [][]byte{}
+
+	for i := uint16(0); i < qdCount; i++ {
+		questionResponse := createQuestionSection(buf[12:])
+		totalQuestions = append(totalQuestions, questionResponse)
+	}
+
+	resolverIP := strings.Split(resolverAddr, ":")[0]
+	resolverPort, err := strconv.Atoi(strings.Split(resolverAddr, ":")[1])
+	if err != nil {
+		fmt.Printf("cannot convert %s port to int", resolverIP)
+	}
+	for _, question := range totalQuestions {
+		newHeader := make([]byte, len(header))
+		copy(newHeader, header)
+		binary.BigEndian.PutUint16(newHeader[4:6], 1)
+		packet := make([]byte, 0)
+		packet = append(packet, newHeader...)
+		packet = append(packet, question...)
+
+		response, n := connectResolver(packet, resolverIP, resolverPort)
+		answerOffset := 12
+		questionLen := createQuestionSection(response[answerOffset:])
+		answerOffset += len(questionLen)
+
+		if answerOffset < n {
+			answer := response[answerOffset:n]
+			if len(answer) > 0 {
+				totalAnswers = append(totalAnswers, answer)
+			}
+		}
+	}
+	return totalAnswers
 }
 
 func main() {
-	fmt.Println("Logs from your program will appear here!")
-
+	fmt.Println("Logs will appear here!")
 	udpAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:2053")
 	if err != nil {
 		fmt.Println("Failed to resolve UDP address:", err)
 		return
 	}
-
+	fmt.Println("resolver", os.Args[2])
 	udpConn, err := net.ListenUDP("udp", udpAddr)
 	if err != nil {
 		fmt.Println("Failed to bind to address:", err)
@@ -200,8 +270,8 @@ func main() {
 		}
 
 		qdcount := binary.BigEndian.Uint16(buf[4:6])
-
-		headerResponse, err := createHeaderSection(buf)
+		answerResponse := forwardDNSServer(buf, os.Args[2])
+		headerResponse, err := createHeaderSection(buf, uint16(len(answerResponse)))
 		if err != nil {
 			fmt.Println("Error creating header:", err)
 			break
@@ -210,20 +280,13 @@ func main() {
 		response := make([]byte, 0)
 		response = append(response, headerResponse...)
 
-		var currentPos uint16 = 12
 		for i := uint16(0); i < qdcount; i++ {
-			questionResponse := createQuestionSection(buf[currentPos:])
+			questionResponse := createQuestionSection(buf[12:])
 			response = append(response, questionResponse...)
-			currentPos += uint16(len(questionResponse))
 		}
-
-		answerResponse, err := createAnswerSection(buf[12:], int(qdcount))
-		if err != nil {
-			fmt.Println("Error creating answer:", err)
-			break
+		for _, answer := range answerResponse {
+			response = append(response, answer...)
 		}
-		response = append(response, answerResponse...)
-
 		_, err = udpConn.WriteToUDP(response, source)
 		if err != nil {
 			fmt.Println("Failed to send response:", err)
